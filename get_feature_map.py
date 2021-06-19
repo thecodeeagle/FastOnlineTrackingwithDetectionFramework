7e927348c4a57bcb8a189c1cefe401fd56330445
thecodeeagle <41933137+thecodeeagle@users.noreply.github.com>
2021-06-19 18:52:40 +0530

    Level 1 Changes

--- get_feature_map.py
+++ get_feature_map.py
@@ -1,69 +1,59 @@
-# Generated with SMOP  0.41
-from libsmop import *
-# .\get_feature_map.m
 
-    
-@function
+
 def get_feature_map(im_patch=None,features=None,w2c=None,*args,**kwargs):
-    varargin = get_feature_map.varargin
-    nargin = get_feature_map.nargin
+
 
     # out = get_feature_map(im_patch, features, w2c)
-    
     # Extracts the given features from the image patch. w2c is the
-# Color Names matrix, if used.
-    
+    # Color Names matrix, if used.
+
     if nargin < 3:
         w2c=[]
-# .\get_feature_map.m:9
-    
+
+
     # the names of the features that can be used
     valid_features=cellarray(['gray','cn'])
-# .\get_feature_map.m:13
+
     # the dimension of the valid features
     feature_levels=concat([1,10]).T
-# .\get_feature_map.m:16
+
     num_valid_features=length(valid_features)
-# .\get_feature_map.m:18
     used_features=false(num_valid_features,1)
-# .\get_feature_map.m:19
+
     # get the used features
     for i in arange(1,num_valid_features).reshape(-1):
         used_features[i]=any(strcmpi(valid_features[i],features))
-# .\get_feature_map.m:23
-    
+
+
     # total number of used feature levels
     num_feature_levels=sum(multiply(feature_levels,used_features))
-# .\get_feature_map.m:27
+
     level=0
-# .\get_feature_map.m:29
+
     # If grayscale image
     if size(im_patch,3) == 1:
         # Features that are available for grayscale sequances
         # Grayscale values (image intensity)
         out=single(im_patch) / 255 - 0.5
-# .\get_feature_map.m:36
+
     else:
         # Features that are available for color sequances
         # allocate space (for speed)
         out=zeros(size(im_patch,1),size(im_patch,2),num_feature_levels,'single')
-# .\get_feature_map.m:41
+
         if used_features(1):
             out[arange(),arange(),level + 1]=single(rgb2gray(im_patch)) / 255 - 0.5
-# .\get_feature_map.m:45
             level=level + feature_levels(1)
-# .\get_feature_map.m:46
+
         # Color Names
         if used_features(2):
             if isempty(w2c):
                 # load the RGB to color name matrix if not in input
                 temp=load('w2crs')
-# .\get_feature_map.m:53
+
                 w2c=temp.w2crs
-# .\get_feature_map.m:54
+
             # extract color descriptor
             out[arange(),arange(),level + (arange(1,10))]=im2c(single(im_patch),w2c,- 2)
-# .\get_feature_map.m:58
+
             level=level + feature_levels(2)
-# .\get_feature_map.m:59
-    
--- get_region.py
+++ get_region.py
@@ -1,49 +1,33 @@
-# Generated with SMOP  0.41
-from libsmop import *
-# .\get_region.m
 
-    
-@function
 def get_region(im=None,pos=None,sz=None,*args,**kwargs):
-    varargin = get_region.varargin
-    nargin = get_region.nargin
+
 
     # [out_npca, out_pca] = get_subwindow(im, pos, sz, non_pca_features, pca_features, w2c)
-    
+
     # Extracts the non-PCA and PCA features from image im at position pos and
 # window size sz. The features are given in non_pca_features and
 # pca_features. out_npca is the window of non-PCA features and out_pca is
 # the PCA-features reshaped to [prod(sz) num_pca_feature_dim]. w2c is the
 # Color Names matrix if used.
-    
+
     if isscalar(sz):
         sz=concat([sz,sz])
-# .\get_region.m:12
-    
+
+
     xs=floor(pos(1)) + (arange(1,sz(1))) - floor(sz(1) / 2)
-# .\get_region.m:15
     ys=floor(pos(2)) + (arange(1,sz(2))) - floor(sz(2) / 2)
-# .\get_region.m:16
+
     #check for out-of-bounds coordinates, and set them to the values at
 #the borders
     xs[xs < 1]=1
-# .\get_region.m:20
     ys[ys < 1]=1
-# .\get_region.m:21
     xs[xs > size(im,2)]=size(im,2)
-# .\get_region.m:22
     ys[ys > size(im,1)]=size(im,1)
-# .\get_region.m:23
     x1=xs(1)
-# .\get_region.m:25
     x2=xs(end())
-# .\get_region.m:26
     y1=ys(1)
-# .\get_region.m:27
     y2=ys(end())
-# .\get_region.m:28
     return x1,y1,x2,y2
-    
+
 if __name__ == '__main__':
-    pass
-    
+    get_region()
