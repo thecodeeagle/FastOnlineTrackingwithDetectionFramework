f0d308ea92062234d6845bd47e07380dc586f168
thecodeeagle <41933137+thecodeeagle@users.noreply.github.com>
2021-06-21 10:29:37 +0530

    Third func updated

--- dense_gauss_kernel.py
+++ dense_gauss_kernel.py
@@ -1,25 +1,29 @@
-def dense_gauss_kernel(sigma=None,x=None,y=None,*args,**kwargs):
+import numpy as np
+
+def dense_gauss_kernel(sigma=20.3,x=[[[2,3], [3,2]]],y=[[[2,3], [3,2]]],*args,**kwargs):
 
     # k = dense_gauss_kernel(sigma, x, y)
     # Computes the kernel output for multi-dimensional feature maps x and y using a Gaussian kernel with standard deviation sigma.
 
-    xf=fft2(x)
+    xf= np.fft.fft2(x)
 
 
-    xx=dot(ravel(x).T,ravel(x))
+    xx= np.dot(np.transpose(np.ravel(x)),np.ravel(x))
 
-    if nargin >= 3:
-        yf=fft2(y)
-        yy=dot(ravel(y).T,ravel(y))
+    if len(kwargs) >= 3:
+        yf= np.fft2(y)
+        yy= np.dot(np.transpose(np.ravel(y)), np.ravel(y))
     else:
-        yf=copy(xf)
-        yy=copy(xx)
+        yf= xf.copy()
+        yy= xx.copy()
 
-    xyf=multiply(xf,conj(yf))
-    xy=real(ifft2(sum(xyf,3)))
+    xyf= np.multiply(xf, np.conj(yf))
+    xy=  np.real(np.fft.ifft2(np.sum(xyf,2)))  #temporary change in axes to suit the example
+    #k=2
 
-    k=exp(dot(- 1 / sigma ** 2,max(0,(xx + yy - dot(2,xy)) / numel(x))))
-    return k
+    k= np.exp(np.dot(- 1 / sigma*sigma,max(0,(xx.all() + yy.all() - np.dot(2,xy.all()))) / len(x))) #check if sigma*sigma is fine substitute for sigma squared
+
+    #return k
 
 if __name__ == '__main__':
     dense_gauss_kernel()
